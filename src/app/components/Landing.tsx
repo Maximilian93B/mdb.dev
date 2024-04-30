0fe535e190c3f34951ad7f26527b33aa0e04f6b2
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-04-29 21:45:04 -0300

    Learning NextJs use client context

--- src/app/components/Landing.tsx
+++ src/app/components/Landing.tsx
@@ -1,22 +1,50 @@
-import React from 'react';
+'use client'
 
+import React, { useEffect } from 'react';
+import { animated, useSpring, SpringValues } from '@react-spring/web';
 
+// Updated to new syntax for client-side execution
 
-const Landing: React.FC = () => {
-return (
-    <div className='landing'>
-        <h1 className='header'>Welcome to my Portfolio</h1>
-        <p className='subheader'> I build full stack apps</p>    
+// Define interface for animation
 
-    <div className='LadningText'>
-        <h1> This was built using NextJs </h1>
-    </div>
-
-    </div>
-    )
+interface AnimatedStyles {
+    opacity: number;
+    transform: string;
 }
 
+const Landing: React.FC = () => {
+    // Define the animation using useSpring
+    const [styles, api] = useSpring<AnimatedStyles>(() => {
+        console.log('setting up animations');
+        return{
+            from: { opacity: 1, transform: 'translateY(-20px)' },
+            to: { opacity: 0, transform: 'translateY(0)' },
+            config: { duration: 1000 }
+        };
+    });
 
+    // Reset the animation every time this component mounts
+    useEffect(() => {
+        console.log('Attempting to start animation.');
+        api.start();
+    }, [api]);
 
+    
+    return (
+        <div className='landing' style={{ position: 'relative', height: '100vh' }}>
+            <animated.h1 style={styles} className='header'>
+                I am a Full Stack Developer and ...
+            </animated.h1>
+            <animated.p style={styles} className='subheader'>
+                ....
+            </animated.p>
+            <div className='LandingText'>
+                <animated.h1 style={styles} className='header'>
+                    This was built using NextJs
+                </animated.h1>
+            </div>
+        </div>
+    );
+}
 
 export default Landing;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react';
 import NavBar from './components/Navbar';
 import About from './components/About';
@@ -5,6 +7,10 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Landing from './components/Landing';
 import './globals.css'
+
+// This line allows NextJs to execute all child components of our 'Home' component on the client side to handle animations etc.. 
+
+
 
 const Home: React.FC = () => {
   return (
